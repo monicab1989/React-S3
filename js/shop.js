a9c04fa3aacc5bc8057c2a37829dc1bf2e3ac458
monicab1989 <monika_291@hotmail.com>
2022-02-24 20:12:49 +0100

    Update exercice 7,8 and 9

--- js/shop.js
+++ js/shop.js
@@ -66,8 +66,9 @@ var total = 0;
 // Exercise 1
 let ulElement = document.getElementsByClassName("list")[0];
 let prices = document.getElementById("prices");
-let totalElement=document.getElementById("total");
-function buy(id) {
+let totalElement = document.getElementById("total");
+
+/*function buy(id) {
   // 1. Loop for to the array products to get the item to add to cart
   for (let i = 0; i < products.length; i++) {
     let product = products[i];
@@ -79,55 +80,43 @@ function buy(id) {
       break;
     }
   }
-}
-
-function paintShoppingCar(){
-  for(let i=0;i<cart.length;i++){
-    
-    let textElement = document.createTextNode(cart[i].quantity + " " + cart[i].name);
-      let liElement = document.createElement("li");
-      liElement.appendChild(textElement);
-      liElement.style.listStyle="none";
-      ulElement.appendChild(liElement);
-      let priceElement = document.createElement("li");
-      let textPrice = document.createTextNode("Price: " + cart[i].price);
-      priceElement.appendChild(textPrice);
-      priceElement.style.listStyle="none";
-      prices.appendChild(priceElement);
-  }
-}
+}*/
 
 // Exercise 2
-let buttonElement = document.createElement("button");
-let textButton = document.createTextNode("Delete");
-let containerElement = document.getElementById("containerList");
-buttonElement.appendChild(textButton);
-buttonElement.style.backgroundColor = "#E5E7E9";
-containerElement.appendChild(buttonElement);
-buttonElement.addEventListener("click", cleanCart, false);
 
 function cleanCart() {
   cartList.length = 0;
+  total = 0;
+ 
   ulElement.innerHTML = "";
-  prices.innerHTML="";
-  total=0;
-  totalElement.innerHTML="";
-
+  prices.innerHTML = "";
+  totalElement.innerHTML = "";
 }
 
 // Exercise 3
 
-function calculateTotal() {
+/*function calculateTotal() {
   // Calculate total price of the cart using the "cartList" array
   total=0;
   for (let i = 0; i < cartList.length; i++) {
     total = total + cartList[i].price;
     totalElement.innerHTML="Total: " + total + "€"; 
   }
+}*/
+
+//Modificado el ejercicio 3 para hacer el 7
+
+function calculateTotal() {
+  // Calculate total price of the cart using the "cartList" array
+  total = 0;
+  for (let i = 0; i < cart.length; i++) {
+    total = total + cart[i].price;
+    totalElement.innerHTML="Total: " + total + "€"; 
+  }
 }
 
 // Exercise 4
-function generateCart() {
+/*function generateCart() {
   // Using the "cartList" array that contains all the items in the shopping cart,
   // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
   let subtotal=0;
@@ -160,18 +149,18 @@ function generateCart() {
     paintShoppingCar(); 
     applyPromotionsCart(); 
   }
-}
+}*/
 
 // Exercise 5
 function applyPromotionsCart() {
   // Apply promotions to each item in the array "cart"
-  for(let i=0; i<cart.length;i++){
-    if(cart[i].quantity>=3 && cart[i].name==="cooking oil"){
+  for (let i = 0; i < cart.length; i++) {
+    if (cart[i].quantity >= 3 && cart[i].name === "cooking oil") {
       cart[i].subtotalWithDiscount = (cart[i].price - 0.5) * cart[i].quantity;
       console.log(cart[i].subtotalWithDiscount);
     }
-    if(cart[i].quantity>=10 && cart[i].name==="Instant cupcake mixture"){
-      cart[i].subtotalWithDiscount = (2 * cart[i].price)/3;
+    if (cart[i].quantity >= 10 && cart[i].name === "Instant cupcake mixture") {
+      cart[i].subtotalWithDiscount = (2 * cart[i].price) / 3;
       console.log(cart[i].subtotalWithDiscount);
     }
   }
@@ -184,15 +173,82 @@ function addToCart(id) {
   // Refactor previous code in order to simplify it
   // 1. Loop for to the array products to get the item to add to cart
   // 2. Add found product to the cart array or update its quantity in case it has been added previously.
+  for (let i = 0; i < products.length; i++) {
+    let product = products[i];
+    if (product.id == id) {
+      cart.push({
+        id: product.id,
+        name: product.name,
+        price: product.price,
+        type: product.type,
+        quantity: 0,
+        subtotal: 0,
+        subtotalWithDiscount: 0,
+      });
+      let productFind = "";
+      for (let j = 0; j < cart.length; j++) {
+        if (cart[j].id === product.id) {
+          productFind = cart[j];
+          ulElement.innerHTML = "";
+      prices.innerHTML = "";
+          break;
+        }
+      }
+      if (productFind === "") {
+        cart.push(productFind);
+      } else {
+        productFind.quantity++;
+        subtotal = productFind.quantity * productFind.price;
+      }
+      applyPromotionsCart();
+      calculateTotal();
+      ulElement.innerHTML = "";
+      prices.innerHTML = "";
+      printCart();
+      
+    }
+  }
 }
 
 // Exercise 8
 function removeFromCart(id) {
   // 1. Loop for to the array products to get the item to add to cart
   // 2. Add found product to the cartList array
+  for(let i=0; i<cart.length; i++){
+    if(cart[i]==id && cart[i].quantity > 1){
+      card[i].quantity--;
+    }else{
+      cart.splice(i,0);
+    }
+  }
 }
 
 // Exercise 9
 function printCart() {
   // Fill the shopping cart modal manipulating the shopping cart dom
+  for(let i=0;i<cart.length;i++){
+     if(cart[i].quantity!=0){
+       let textElement = document.createTextNode(cart[i].quantity + " " + cart[i].name);
+      let liElement = document.createElement("li");
+      liElement.appendChild(textElement);
+      liElement.style.listStyle="none";
+      ulElement.appendChild(liElement);
+      let priceElement = document.createElement("li");
+      let textPrice = document.createTextNode("Price: " + cart[i].price);
+      priceElement.appendChild(textPrice);
+      priceElement.style.listStyle="none";
+      prices.appendChild(priceElement);
+     }
+      
+
+
+      
+  }
 }
+let buttonElement = document.createElement("button");
+      let textButton = document.createTextNode("Delete");
+      let containerElement = document.getElementById("containerList");
+      buttonElement.appendChild(textButton);
+      buttonElement.style.backgroundColor = "#E5E7E9";
+      containerElement.appendChild(buttonElement);
+      buttonElement.addEventListener("click", cleanCart, false);
